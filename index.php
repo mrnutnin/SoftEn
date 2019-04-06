84193e990a6e6c3d336ce705054a697f33f33e14
porinut <porinut1@gmail.com>
2019-04-06 16:13:12 +0700

    Check Login Complete

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <html>
 <head>
 <link rel="Stylesheet" type="text/css" href="Components/bootstrapIndex.css">
@@ -7,17 +9,20 @@
 	function validation(){ 
 		var username = document.myform.username.value;
 		var password = document.myform.password.value;
-    		if (username == "" && password == "") {
-     	document.getElementById('errors').innerHTML="*Please enter your Username and Password!!";
+    	if (username == "" && password == "") {
+     	    //document.getElementById('errors').innerHTML="*Please enter your Username and Password!!";
+             alert('*Please enter your Username and Password!!');
      	return false;
  		} else if (username == "") {
- 			document.getElementById('errors').innerHTML="*Please enter your Username!!";
+ 			//document.getElementById('errors').innerHTML="*Please enter your Username!!";
+             alert('*Please enter your Username!!');
      	return false;
 		} else if (password == "") {
-      document.getElementById('errors').innerHTML="*Please enter your Password!!";
+           // document.getElementById('errors').innerHTML="*Please enter your Password!!";
+           alert('*Please enter your Password!!');
      	return false;
-    }
-}		
+        }
+    }		
 </script> 
 
 </head>
@@ -27,7 +32,7 @@
   <div id="formContent">
     <!-- Login Form -->
     <h1>Sign In</h1>
-    <form method="POST" action="Models/Login.php" name="myform" onsubmit="return validation();">
+    <form method="POST" action="Models/CheckLogin.php" name="myform" onsubmit="return validation();">
       <input  name="username" placeholder="username"  type="text" id="username">
       <input  name="password" placeholder="password"  type="password" id="password">
       <input type="submit"value="Sign In" name="signin">
